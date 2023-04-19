3509e822192306c33d87a197e1a78f6d5a172310
Kosmina Ivan <kosminaivanm@gmail.com>
2023-04-20 01:04:29 +0300

    Init commit

---
+++ app/src/main/java/com/gmail/at/kosminaivan/notebook/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gmail.at.kosminaivan.notebook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
